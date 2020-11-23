7bd0d7efcad1dec9f0414b12d4da7a596718dff2
kirillnev <mail@kirillnev.ru>
2020-11-23 11:12:00 +0300

    First Calculator commit

--- my.js
+++ my.js
@@ -1,12 +1,29 @@
-var myHeading = document.querySelector('h1');
-
-var a = 6
-if(a === 6){
-    myHeading.textContent = 'Hello 6 world!';
+function addition(num1, num2) {
+    return num1 + num2;
+}
+function subtraction(num1, num2) {
+    return num1 - num2;
+}
+function multiplication(num1, num2) {
+    return num1 * num2;
+}
+function division(num1, num2) {
+    try {
+        return num1 / num2;
+    } catch (e) {
+        console.log("DevisionByZero");
+    }
 }
 
-myHeading.onclick = function(){
-    var myName = prompt('Please enter your name: ');
-    myHeading.textContent = 'Hello , ' + myName;
-
+function getResult(num1, num2, operation){
+    return operation(num1, num2);
 }
+function showResult(result){
+    var resultFiled = document.getElementById("operationResult");
+    resultFiled.innerText = result;
+}
+
+var operationElement = document.getElementById("operations");
+operationElement.onchange = function(){
+    alert(this.value);
+};
